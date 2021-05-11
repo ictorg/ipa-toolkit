be34e76989e636c24fd11f3bf43cda3062ea7554
Robin <1105080+openscript@users.noreply.github.com>
2021-05-11 05:15:40 +0000

    Scaffold authentication

--- app/graphql/workspace_schema.rb
+++ app/graphql/workspace_schema.rb
@@ -1,4 +1,12 @@
 class WorkspaceSchema < GraphQL::Schema
+  use GraphqlDevise::SchemaPlugin.new(
+    query:            Types::QueryType,
+    mutation:         Types::MutationType,
+    resource_loaders: [
+      GraphqlDevise::ResourceLoader.new(User)
+    ]
+  )
+
   mutation(Types::MutationType)
   query(Types::QueryType)
 
