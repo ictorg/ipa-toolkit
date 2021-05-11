12afc8d5ac6207dbe51a39731ef01685bfe04791
Robin <1105080+openscript@users.noreply.github.com>
2021-05-11 16:53:44 +0000

    Add seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@
 
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :confirmable, :lockable, :timeoutable, :trackable, :omniauthable, :registerable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
   include GraphqlDevise::Concerns::Model
 end
