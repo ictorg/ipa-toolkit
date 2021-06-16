fdc4c2ad5c3eadf550602a28e68a6fb7e67dcc28
Robin <1105080+openscript@users.noreply.github.com>
2021-06-16 17:56:55 +0000

    Fix participant duplicates

--- app/services/conference_service.rb
+++ app/services/conference_service.rb
@@ -13,12 +13,13 @@ class ConferenceService < ApplicationService
   def save
     parsed_conference = @conference.to_h.transform_keys { |key| CONFERENCE_MAPPINGS[key] || key }
     current_conference = Conference.find_or_initialize_by(id: parsed_conference[:id])
+
+    current_conference.assign_attributes(parsed_conference)
     unless parsed_conference[:participants_attributes].nil?
       current_conference.participants = parsed_conference[:participants_attributes].map do |p|
         Participant.find_or_create_by(p)
       end
     end
-    current_conference.assign_attributes(parsed_conference)
     current_conference.save!
     current_conference.as_json(include: :participants)
   end
