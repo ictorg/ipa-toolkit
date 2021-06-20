e95338c7f6c1a46305fe0f22410e2130b23c5ef2
Robin <1105080+openscript@users.noreply.github.com>
2021-06-20 18:06:19 +0000

    Enhance invite email

--- app/mailers/verification_mailer.rb
+++ app/mailers/verification_mailer.rb
@@ -8,6 +8,6 @@ class VerificationMailer < ApplicationMailer
     @base_path = Rails.env.development? ? 'http' : 'https'
     @base_path += "://#{ENV.fetch('API_ORIGIN', 'localhost:3000')}"
 
-    mail(to: @verification.participant.email, subject: 'Einladung zur Verifizierung')
+    mail(to: @verification.participant.email, subject: "IPA Toolkit: Einladung zur Verifizierung")
   end
 end
